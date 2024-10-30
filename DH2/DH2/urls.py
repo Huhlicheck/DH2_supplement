44e9de2eb7358a626702505fc558dcd4bf707d5d
Petr Sýkora <sejcaaa@gmail.com>
2024-10-30 23:56:03 +0100

    created character details

--- DH2/DH2/urls.py
+++ DH2/DH2/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("polls/", include("characters.urls")),
+    path("characters/", include("characters.urls")),
     path("admin/", admin.site.urls),
 ]
